0e009a9dc21e76e9554c9f109738db1687d4ee65
Mario Cortes <mcortes.1113@gmail.com>
2019-12-12 14:36:01 -0600

    added / route for deployment

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 // app.use(express.static(__dirname + '/public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public', 'db.json'));
   // fs.readFile()
