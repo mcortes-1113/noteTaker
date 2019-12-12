8761fbbc6cd8a259ba06af87246b54392a1dc6d4
Mario Cortes <mcortes.1113@gmail.com>
2019-12-11 22:32:25 -0600

    code updates to solve functionality bugs

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var express = require("express");
 var path = require("path");
-// const apiRoutes = require('./routes/apiRoutes');
-// const htmlRoutes = require('./routes/htmlRoutes');
 
 // Sets up the Express App
 // =============================================================
@@ -12,36 +10,26 @@ var PORT = process.env.PORT||3600;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('/public'));
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use(express.static(__dirname + '/public'));
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "notes.html"));
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public', 'db.json'));
 });
 
-app.post('/api/notes', (req, res) => {
-  var newNote = req.body;
-  // var data = fs.readFileSync('db.json');
-  // var json = JSON.parse(data);
-  // json.push();
-  // fs.writeFile("db.json", JSON.stringify(json))
-  res.json(newNote);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
 });
+
+// app.post('/api/notes', (req, res) => {
+//   res.json(newNote);
+// });
 
 // app.delete('/api/notes/:id', (req, res) => {
-  // var json = { ... };
-  // var key = "foo";
-  // delete json[key];
 // })
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
